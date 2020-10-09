f679a762bcf168e998940fa7f9ae4bc3a656a0ff
givemo <gtchiwanza@gmail.com>
2020-10-09 19:29:53 +0100

    initial commit

---
+++ js/sight.js
@@ -0,0 +1,10 @@
+const { tsThisType } = require("jscodeshift");
+
+class SVGElement {
+  constructor(type) {
+    this.type = type;
+    this.namespace = "http://www.w3.org/2000/svg";
+    this.node = document.createAttributeNS(namespace, type);
+    return this;
+  }
+}
